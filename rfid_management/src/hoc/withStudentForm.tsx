a6c19245704456ca4d737ab3e83a6a1754b74774
Ashim Banerjee <125278221+210310624729@users.noreply.github.com>
2025-12-08 11:47:32 +0530

    Update withStudentForm.tsx

--- rfid_management/src/hoc/withStudentForm.tsx
+++ rfid_management/src/hoc/withStudentForm.tsx
@@ -9,6 +9,8 @@ interface Props {
 
 function withStudentForm<T extends Props>(WrappedComponent: React.ComponentType<T>) {
   return function FormWrapper(props: T) {
+    const [errors, setErrors] = useState<any>({});
+
     const [formData, setFormData] = useState<Student>(
       props.initialData || {
         firstName: "",
@@ -23,11 +25,39 @@ function withStudentForm<T extends Props>(WrappedComponent: React.ComponentType<
     );
 
     const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+
+       const { name, value } = e.target;
+
+   if (!validateField(name, value)) {
+    return;
+  }
       setFormData(prev => ({
         ...prev,
         [e.target.name]: e.target.value,
       }));
     };
+
+    function validateField(key: string, value: string) {
+  switch (key) {
+    case "contact":
+      return /^[0-9]{0,10}$/.test(value); 
+
+    case "roll":
+      return /^[0-9]*$/.test(value); 
+
+    case "grade":
+      return /^(1[0-2]|[1-9])$/.test(value) || value === ""; 
+    case "section":
+      return /^[A-Z]$/.test(value) || value === ""; 
+
+    case "age":
+      return /^[0-9]*$/.test(value) && Number(value) <= 100;
+
+    default:
+      return true; 
+  }
+}
+
 
     const handleSubmit = (e: React.FormEvent) => {
       e.preventDefault();
